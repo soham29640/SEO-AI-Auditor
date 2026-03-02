44c5ec6364c1030ea4492c2b221f7ce3683ec34e
soham29640 <soham04x18@gmail.com>
2026-03-02 20:52:04 +0530

    initial commitment

---
+++ src/tfidf_vectorizer.py
@@ -0,0 +1,12 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def build_tfidf(text_series):
+
+    tfidf = TfidfVectorizer(
+        max_features=3000,
+        ngram_range=(1,3)
+    )
+
+    X = tfidf.fit_transform(text_series)
+
+    return X, tfidf
